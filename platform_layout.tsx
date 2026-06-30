fc90aa62a34f5883623f63cdf9ce7b1b0f39745a
Punyashree KS <shreekspunya@gmail.com>
2026-06-30 16:21:37 +0530

    render children inside RootLayout for nested route composition

--- platform_layout.tsx
+++ platform_layout.tsx
@@ -25,3 +25,4 @@ export default function RootLayout({
 	   <ThemeProvider attribute="class" defaultTheme="light" enableSystemo>
 	     <AuthProvider>
 	       <LanguageProvider>
+	          {children}
