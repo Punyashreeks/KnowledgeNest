fd008f7496d19449d81af1319bf67b73e284e612
Punyashree KS <shreekspunya@gmail.com>
2026-06-30 20:56:32 +0530

    handle form submission based on active authentication tab

--- authentication-form.tsx
+++ authentication-form.tsx
@@ -97,3 +97,5 @@ const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         </CardTitle>
 	</CardHeader>
       <CardContent>
+       <form
+          onSubmit={tab === "login" ? handleLogin : handleRegister}
